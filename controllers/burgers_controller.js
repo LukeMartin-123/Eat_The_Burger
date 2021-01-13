0acd139a3f92fec6123aaca433f69965c7ecdb4b
Luke Martin <ljmart02@gmail.com>
2021-01-12 21:31:20 -0700

    Updates to the routes and db name

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,8 +3,43 @@ var router = express.Router();
 
 var burgers = require("../models/burgers.js");
 
-
-
+router.get("/", function(req, res) {
+    burgers.all(function(data) {
+      var hbsObject = {
+        burgers: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
+  
+  router.post("/api/burgers", function(req, res) {
+    burgers.create([
+      "burger_name", "devoured"
+    ], [
+      req.body.burger_name, req.body.devoured
+    ], function(result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    });
+  });
+  
+  router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+    burgers.update({
+      devoured: req.body.devoured
+    }, condition, function(result) {
+      if (result.changedRows == 0) {
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
+  
 
 
 
